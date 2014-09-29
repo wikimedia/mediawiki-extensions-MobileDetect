af506dc4a6f2185c7cbc82ddb579d32002e9055d
Luis Felipe Schenone <schenonef@gmail.com>
2014-09-29 11:18:00 +0800

    Updated and enhanced the extension
    This extension was only available in-wiki at
    https://www.mediawiki.org/wiki/Extension:MobileDetect
    I'm uploading it to Gerrit for the first time, after updating
    its code structure, while keeping backwards compatibility,
    and enhancing it with the tags <nomobile> and <mobileonly>
    
    Change-Id: I85cca8345fcbd27499508ad687028771f06766ca

---
+++ MobileDetect.i18n.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Internationalisation for MobileDetect extension
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Luis Felipe Schenone
+ */
+$messages['en'] = array(
+	'mobiledetect-desc' => 'Detects mobile devices and allows to control the content visible in each case with the &lt;nomobile&gt; and &lt;mobileonly&gt; tags.',
+);
+
+/** Message documentation (Message documentation)
+ * @author Luis Felipe Schenone
+ */
+$messages['qqq'] = array(
+	'mobiledetect-desc' => '{{desc|name=MobileDetect|url=https://www.mediawiki.org/wiki/Extension:MobileDetect}}',
+);
+
+/** Spanish (español)
+ * @author Luis Felipe Schenone
+ */
+$messages['es'] = array(
+	'mobiledetect-desc' => 'Detecta dispositivos móbiles y permite controlar el contenido visible en cada caso con las etiquetas &lt;nomobile&gt; y &lt;mobileonly&gt;',
+);

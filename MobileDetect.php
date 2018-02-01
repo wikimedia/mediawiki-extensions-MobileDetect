48e74b15679b39110cd3f4aff3abf184b6e8554f
Kunal Mehta <legoktm@member.fsf.org>
2018-01-31 23:20:07 -0800

    Use SPDX 3.0 license identifier
    SPDX released version 3 of their license list (<https://spdx.org/licenses/>),
    which changed the FSF licenses to explicitly end in -only or -or-later
    instead of relying on an easy to miss + symbol.
    
    Bug: T183858
    Change-Id: I50a47360fb09a055a1004a594f49f235a4ea14ac

--- MobileDetect.php
+++ MobileDetect.php
@@ -3,7 +3,7 @@
 $wgExtensionCredits['parserhook'][] = array(
 	'name' => 'MobileDetect',
 	'version' => 2.0,
-	'license-name' => 'GPL-2.0+',
+	'license-name' => 'GPL-2.0-or-later',
 	'descriptionmsg' => 'mobiledetect-desc',
 	'author' => array( 'Matthew Tran', 'Luis Felipe Schenone' ),
 	'url' => 'https://www.mediawiki.org/wiki/Extension:MobileDetect',
